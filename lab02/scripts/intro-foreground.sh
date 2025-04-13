ddb8e6086676c76acd41aa5e64873caa49f2ea4f
David Chung <dchung@dchung.com>
2025-04-13 02:40:24 -0500

    path

--- lab02/scripts/intro-foreground.sh
+++ lab02/scripts/intro-foreground.sh
@@ -26,11 +26,16 @@ do
      echo -n '.' > /dev/tty 2>&1
 done
 
-/home/cassandra-user/cassandra/bin/cqlsh -e "DESCRIBE KEYSPACES;" > /dev/tty 2>&1
+
 
 echo -e "\n\n" > /dev/tty 2>&1
 echo -e " Lab environment ready!" > /dev/tty 2>&1
 echo -e "\n" > /dev/tty 2>&1
+
+EXPORT PATH="/home/cassandra-user/cassandra/bin:$PATH"
+
+su cassandra-user 
+cd /home/cassandra-user
 
 PS1="\w $ "
 
