0fff3b74aea3f55a4b354fabd196ad2b720bb8fe
David Chung <dchung@dchung.com>
2025-04-13 02:24:26 -0500

    debug

--- lab02/scripts/intro-foreground.sh
+++ lab02/scripts/intro-foreground.sh
@@ -20,21 +20,35 @@ echo " ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══�
 
 echo -ne "\n\n Configuring the lab envronment " > /dev/tty 2>&1
 
-until /home/cassandra-user/cassandra/bin/cqlsh -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1;
+until [[ -e /usr/lib/jvm && -e /usr/bin/java ]]
 do
      sleep 1
      echo -n '.' > /dev/tty 2>&1
 done
 
 echo -e "\n\n" > /dev/tty 2>&1
-echo -e " Lab environment ready!\n\n" > /dev/tty 2>&1
+echo -e " Lab environment ready!" > /dev/tty 2>&1
 echo -e "\n" > /dev/tty 2>&1
-
-EXPORT PATH="/home/cassandra-user/cassandra/bin:$PATH"
-
-su cassandra-user 
-cd /home/cassandra-user
 
 PS1="\w $ "
 
 exec > /dev/tty 2>&1
+
+# until /home/cassandra-user/cassandra/bin/cqlsh -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1;
+# do
+#      sleep 1
+#      echo -n '.' > /dev/tty 2>&1
+# done
+
+# echo -e "\n\n" > /dev/tty 2>&1
+# echo -e " Lab environment ready!\n\n" > /dev/tty 2>&1
+# echo -e "\n" > /dev/tty 2>&1
+
+# EXPORT PATH="/home/cassandra-user/cassandra/bin:$PATH"
+
+# su cassandra-user 
+# cd /home/cassandra-user
+
+# PS1="\w $ "
+
+# exec > /dev/tty 2>&1
