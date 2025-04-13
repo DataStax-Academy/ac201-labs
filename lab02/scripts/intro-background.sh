0ed613699f2656b620dfa25b571846bd065bdf47
David Chung <dchung@dchung.com>
2025-04-12 21:55:36 -0500

    scripts

--- lab02/scripts/intro-background.sh
+++ lab02/scripts/intro-background.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+groupadd cassandra
+useradd -m -s /bin/bash -g cassandra cassandra-user
+
+apt-get update
+
+sudo apt-get install -y openjdk-11-jdk-headless < /dev/null > /dev/null 
 
 
 
