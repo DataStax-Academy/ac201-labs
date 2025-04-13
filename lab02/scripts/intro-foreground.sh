9002d96d18875d3449e9e164104837b6dbc03025
David Chung <dchung@dchung.com>
2025-04-13 02:48:16 -0500

    script

--- lab02/scripts/intro-foreground.sh
+++ lab02/scripts/intro-foreground.sh
@@ -32,30 +32,11 @@ echo -e "\n\n" > /dev/tty 2>&1
 echo -e " Lab environment ready!" > /dev/tty 2>&1
 echo -e "\n" > /dev/tty 2>&1
 
-EXPORT PATH="/home/cassandra-user/cassandra/bin:$PATH"
+# Start an interactive shell as cassandra-user
+su - cassandra-user -c '
+  export PATH="/home/cassandra-user/cassandra/bin:$PATH"
+  export PS1="\w \$ "
+  cd /home/cassandra-user
+  exec bash
+'
 
-su cassandra-user 
-cd /home/cassandra-user
-
-PS1="\w $ "
-
-exec > /dev/tty 2>&1
-
-# until /home/cassandra-user/cassandra/bin/cqlsh -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1;
-# do
-#      sleep 1
-#      echo -n '.' > /dev/tty 2>&1
-# done
-
-# echo -e "\n\n" > /dev/tty 2>&1
-# echo -e " Lab environment ready!\n\n" > /dev/tty 2>&1
-# echo -e "\n" > /dev/tty 2>&1
-
-# EXPORT PATH="/home/cassandra-user/cassandra/bin:$PATH"
-
-# su cassandra-user 
-# cd /home/cassandra-user
-
-# PS1="\w $ "
-
-# exec > /dev/tty 2>&1
