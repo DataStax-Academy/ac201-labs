10ee3e23858c475110ad3236fba96817429b9c48
David Chung <dchung@dchung.com>
2025-04-12 21:23:16 -0500

    background script

--- lab02/scripts/intro-background.sh
+++ lab02/scripts/intro-background.sh
@@ -6,6 +6,7 @@ useradd -m -s /bin/bash -g cassandra cassandra-user
 apt-get update
 
 sudo apt-get install -y openjdk-11-jdk-headless < /dev/null > /dev/null 
+
 su cassandra-user
 cd /home/cassandra-user
 curl https://dlcdn.apache.org/cassandra/5.0.4/apache-cassandra-5.0.4-bin.tar.gz \
@@ -13,6 +14,8 @@ curl https://dlcdn.apache.org/cassandra/5.0.4/apache-cassandra-5.0.4-bin.tar.gz 
 tar xf apache-cassandra-5.0.4-bin.tar.gz
 rm apache-cassandra-5.0.4-bin.tar.gz
 mv apache-cassandra-5.0.4 cassandra
+cd cassandra/bin
+./cassandra
 
 
 
