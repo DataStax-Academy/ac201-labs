008fecbbbeb792c2cbc40eaad7e07759d26b742d
David Chung <dchung@dchung.com>
2025-04-12 21:41:48 -0500

    more background experimentation

--- lab02/scripts/intro-background.sh
+++ lab02/scripts/intro-background.sh
@@ -14,7 +14,6 @@ curl https://dlcdn.apache.org/cassandra/5.0.4/apache-cassandra-5.0.4-bin.tar.gz 
 tar xf apache-cassandra-5.0.4-bin.tar.gz
 rm apache-cassandra-5.0.4-bin.tar.gz
 mv apache-cassandra-5.0.4 cassandra
-cd cassandra/bin
 
 
 
