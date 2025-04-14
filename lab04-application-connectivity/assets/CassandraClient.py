4520d0c2e41ce050be5213d303d5eb07e095911c
David Chung <dchung@dchung.com>
2025-04-14 15:20:45 -0500

    Python

---
+++ lab04-application-connectivity/assets/CassandraClient.py
@@ -0,0 +1,18 @@
+from cassandra.cluster import Cluster
+
+# Connect to the Cassandra cluster use the keyspace 'cars'
+cluster = Cluster(['172.30.1.10']) 
+session = cluster.connect()
+session.set_keyspace('world') 
+
+# Query the inventory table
+query = "SELECT id, continent, countrt FROM countries"
+rows = session.execute(query)
+
+# Display the results
+print("Countries:")
+for row in rows:
+    print(f"ID: {row.id:<4} Continent: {row.continent:<15} Country: {row.country}")
+
+# Close the connection
+cluster.shutdown()
