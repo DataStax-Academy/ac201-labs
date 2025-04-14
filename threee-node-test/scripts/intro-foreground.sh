e6b2d8b14ff60dc114a3aabc97ab2c18a50aed76
David Chung <dchung@dchung.com>
2025-04-14 01:42:16 -0500

    wait

--- threee-node-test/scripts/intro-foreground.sh
+++ threee-node-test/scripts/intro-foreground.sh
@@ -20,14 +20,14 @@ echo " ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══�
 
 echo -ne "\n\n Configuring the lab environment " > /dev/tty 2>&1
 
-# until \
-#   /home/cassandra-user/nodeA/bin/cqlsh -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1 && \
-#   /home/cassandra-user/nodeB/bin/cqlsh -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1 && \
-#   /home/cassandra-user/nodeC/bin/cqlsh -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1
-# do
-#      sleep 1
-#      echo -n '.' > /dev/tty 2>&1
-# done
+until \
+  /home/cassandra-user/nodeA/bin/cqlsh 172.30.1.10 9042 -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1 && \
+  /home/cassandra-user/nodeB/bin/cqlsh 172.30.1.11 9043 -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1 && \
+  /home/cassandra-user/nodeC/bin/cqlsh 172.30.1.12 9044 -e "DESCRIBE KEYSPACES;" > /dev/null 2>&1
+do
+     sleep 1
+     echo -n '.' > /dev/tty 2>&1
+done
 
 echo -e "\n\n" > /dev/tty 2>&1
 echo -e " Lab environment ready!" > /dev/tty 2>&1
